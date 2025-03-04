d18f5a6d8ca27187b5880ddaf2989de7ca63359d
Aditya <adiajay2003@gmail.com>
2025-03-04 19:26:05 +0530

    Added auth backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+// import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+	fullName: {
+		type: String,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+	},
+	gender: {
+		type: String,
+		required: true,
+		enum: ["male", "female"],
+	},
+	profilePic: {
+		type: String,
+		default: "",
+	}
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
